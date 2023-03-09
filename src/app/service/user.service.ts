d4635ccfa5d63da0e57334cce1ebd389feef315d
Safiullah Omar <omar.safiullah@gmail.com>
2023-03-09 11:45:11 +0430

    login portion is completed for users

--- src/app/service/user.service.ts
+++ src/app/service/user.service.ts
@@ -1,13 +1,13 @@
 import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
+import { EventEmitter, Injectable } from '@angular/core';
 import { Router } from '@angular/router';
-import { SignUp } from '../dataType';
+import { Login, SignUp } from '../dataType';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
-
+  invalidUserAuth= new EventEmitter<boolean>(false);
   constructor(private http:HttpClient,private route:Router) { }
   userSignUp(user:SignUp){
     this.http.post(`http://localhost:3000/user`,user,{observe:'response'}).
@@ -23,6 +23,20 @@ export class UserService {
     if(localStorage.getItem('user')){
       this.route.navigate(['/']);
     }
+  }
+
+  userLogin(data:Login){
+    this.http.get<SignUp[]>(`http://localhost:3000/users?email=${data.email}&password=${data.password}`,
+    {observe:'response'}
+    ).subscribe((result)=>{
+      if(result && result.body?.length){
+        localStorage.setItem('user',JSON.stringify(result.body[0]));
+        this.route.navigate(['/']);
+        this.invalidUserAuth.emit(false)
+      }else{
+        this.invalidUserAuth.emit(true)
+      }
+    })
   }
 
 }
--- src/app/user-auth/user-auth.component.ts
+++ src/app/user-auth/user-auth.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { SignUp } from '../dataType';
+import { Component, OnInit } from '@angular/core';
+import { Login, product, SignUp } from '../dataType';
 import { ProductService } from '../service/product.service';
 import { UserService } from '../service/user.service';
 
@@ -8,12 +8,66 @@ import { UserService } from '../service/user.service';
   templateUrl: './user-auth.component.html',
   styleUrls: ['./user-auth.component.css']
 })
-export class UserAuthComponent {
+export class UserAuthComponent implements OnInit {
+  showLogin:boolean=true
+  authError:string="";
   constructor(private user: UserService, private product:ProductService) {}
+
   ngOnInit(): void {
     this.user.userAuthReload();
   }
   signUp(data: SignUp) {
     this.user.userSignUp(data);
+  }
+  login(data: Login) {
+    this.user.userLogin(data)
+    this.user.invalidUserAuth.subscribe((result)=>{
+      console.warn(result);
+      if(result){
+         this.authError="User not found"
+      }else{
+        this.localCartToRemoteCart();
+      }
+      
+    })
+  }
+  openSignUp(){
+    this.showLogin=false
+  }
+  openLogin(){
+this.showLogin=true;
+  }
+
+  localCartToRemoteCart(){
+   let data = localStorage.getItem('localCart');
+   let user = localStorage.getItem('user');
+   let userId= user && JSON.parse(user).id;
+   if(data){
+    let cartDataList:product[]= JSON.parse(data);
+  
+    cartDataList.forEach((product:product, index)=>{
+      let cartData:cart={
+        ...product,
+        productId:product.id,
+        userId
+      }
+      delete cartData.id;
+      setTimeout(() => {
+        this.product.addToCart(cartData).subscribe((result)=>{
+          if(result){
+            console.warn("data is stored in DB");
+          }
+        })
+      }, 500);
+      if(cartDataList.length===index+1){
+        localStorage.removeItem('localCart')
+      }
+    })
+   }
+
+   setTimeout(() => {
+    this.product.getCartList(userId)
+   }, 2000);
+    
   }
 }
